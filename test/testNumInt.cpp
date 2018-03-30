df0769459fc0d36eefcafa00beae67f96994275a
Lars Flæten <lars.flaeten@gmail.com>
2018-03-30 23:13:40 +0200

    Add test for hyperbolic asymptotic behaviour

--- test/testNumInt.cpp
+++ test/testNumInt.cpp
@@ -96,9 +96,52 @@ TEST_F(NumIntTest, RKF45BenchMarkTest)
     astro::RKF45::setTolerance(1.0E-8);       
 
     auto resv = pr.doSteps(state0, et0, et1, astro::TimeDelta(DT));
+}
+
+TEST_F(NumIntTest, RKF78HypAsymptote)
+{
+    astro::State s;
+    s.r = vec3d(6378 + 400, 0.0, 0.0);
+    //double v = 7.66895; // Circular
+    double v = 10.84509; // roughly eccentric (to six decimals on e)
+    s.v = vec3d(0, 0, v*1.1);
+    
+
+    astro::OrbitElements oe = astro::OrbitElements::fromStateVectorOE(s, et0, mu_earth);
+    //std::cout << oe << std::endl; 
+
+    // Propagate the orbit for a long time (12 days), far outside SOI:
+    astro::EphemerisTime et1 = et0 + 12*60*60*24;
+
+    astro::Propagator<astro::RKF78, astro::RKF78::Result> pr(ode0);
+    astro::RKF78::setTolerance(1.0E-8);       
+
+    double DT = 1.0; // Initial dt
+    auto resv = pr.doSteps(s, et0, et1, astro::TimeDelta(DT));
+    
+    //for(auto res : resv)
+    //    std::cout << res.dt_next.value << "\t" << res.s.r.length() << "\t" << res.s.v.length() << std::endl;
+
+    astro::State s_inf = resv.back().s;
+
+    double v_inf = s_inf.v.length();
+    ASSERT_LT(fabs(v_inf- astro::hyperbolicExcessVelocity(mu_earth, oe.a)), 0.016);
+    
+    // asymptotic angles are more easy based on velocity (fast convergence)
+    // Position based asymptote is much more slow-convergent (about same as v_inf convergence above)
+    double hyp_asym = astro::hyperbolicAsymptote(oe.e);
+    vec3d v0 = -s.r.normalize();
+    vec3d v1 = s_inf.v.normalize();
+    vec3d v2 = s_inf.r.normalize();
+    double hyp_asym1 = acos(v0.dotproduct(v1)); // Asymptote from velocity
+    double hyp_asym2 = acos(v0.dotproduct(v2)); // Asymptote from position
+  
+    ASSERT_LT(fabs(hyp_asym - hyp_asym1), 0.0005); // 0.03 degrees..
+    ASSERT_LT(fabs(hyp_asym - hyp_asym2), 0.0032); // 0.2 degrees..
 
 
-    	
+
+
 
 }
 
