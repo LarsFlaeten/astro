a1542d225b914a1dbc2d84b9fe51e12fb22ab937
Lars Flæten <lars.flaeten@gmail.com>
2018-04-23 22:35:24 +0200

    Add ODE for rotations

--- astro/ODE.cpp
+++ astro/ODE.cpp
@@ -1,5 +1,9 @@
 #include "ODE.h"
 
+// References:
+// [1]  Spacecraft Attitude Dynamics and Control
+//      Ver 3.0.0, 2008/2009 Giulio Avanzini
+
 namespace astro
 {
 
@@ -48,6 +52,41 @@ void ODE::operator()(const PosState& x, PosState& dxdt, const EphemerisTime& et)
 
 }
  
+RotODE::RotODE()
+{
 
+}
+
+RotODE::~RotODE()
+{
+
+}
+
+RotState RotODE::rates(const EphemerisTime& et, const RotState& rs) const
+{
+    RotState rs_dot;
+
+    quatd Q = rs.q;
+    double q0 = Q.w; // w is the scalar in ork/quatd
+    vec3d q = vec3d(Q.x, Q.y, Q.z);
+
+    // Derivative of the quaternion
+    // From [1]
+    vec3d w = rs.w;
+    double q0dot = -0.5*w.dotproduct(q);
+    vec3d qdot = 0.5*(q0*w - w.crossProduct(q));
+    quatd Qdot = quatd(qdot.x, qdot.y, qdot.z, q0dot);
+    rs_dot.q = Qdot;
+
+    // Derivative of w
+    // From torques etc
+    // TODO: Implement. We then need inertial tensor and setters
+    // for torque (body fixed and global)
+    //rs.dot.w = I_inv*(torque +++ ) or somehting
+    rs_dot.w = vec3d::ZERO;
+    
+
+    return rs_dot;
+}
 
 }
--- astro/ODE.h
+++ astro/ODE.h
@@ -7,7 +7,14 @@
 namespace astro
 {
 
-
+// This is the differential equation for translation. Translation is separated
+// from rotation as these will be integrated separately:
+// - They are not coupled or at least lightly coupled
+// - They wil have very different optimal time steps. E.g. an hyperbolic escape
+//   trajectory from earth with RKF78 will have dt ~ 1.0E6 / 1.0E7 @ SOI, while
+//   rotation may need to be integrated with dt many orders below this
+// - This also allows different propagation regimes, for example numerical integration
+//   for rotations, while using Kepler/conincs for translation.
 class ODE
 {
 public:
@@ -28,6 +35,18 @@ public:
 private:
     double mu;
 };
+
+class RotODE
+{
+public:
+    RotODE();
+    virtual ~RotODE();
+
+    // The force function for rotations
+    RotState rates(const EphemerisTime& et, const RotState& s) const;
+
+};
+
 
 }
 
